a23dcc3b2c7220f838a02f2aa93b0a3410b941f1
M-UNSA <mcentenoqu@unsa.edu.pe>
2021-07-29 05:02:43 -0500

    Cambiado los modelos DecimalField para Edad

--- personas/models.py
+++ personas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombres = models.TextField()
-    apellidos = models.TextField()
-    edad = models.TextField()
+    nombre = models.CharField(max_length = 100)
+    apellidos = models.CharField(max_length = 100)
+    edad = models.DecimalField(max_digits=3,decimal_places=0)
+    donador = models.BooleanField()
