a1edfa243840ae591eb9c3ee394ee82bc2299bb2
M-UNSA <mcentenoqu@unsa.edu.pe>
2021-07-29 05:13:38 -0500

    Ruteo de página another a página distinta

--- listaContactos/urls.py
+++ listaContactos/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
+from inicio.views import anotherView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', myHomeView, name='Página de Inicio'),
-    path('another/', myHomeView, name='Página de Inicio'),
+    path('another/', anotherView, name='Página de Inicio'),
 ]
